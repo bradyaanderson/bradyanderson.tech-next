e4bb8cd67ab7ce47b4bcec15fd8ba4c014a111fe
Brady Anderson <bradya48@gmail.com>
2021-06-21 00:02:06 -0500

    Basic landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,38 @@
-import axios from 'axios';
-import React, {useState} from 'react';
+import React from 'react';
+import Image from 'react-bootstrap/Image'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './assests/stylesheets/App.css';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  React.useEffect(() => {
-    axios.get("/test").then(response => setData(response.data.test));
-  }, []);
-
   return (
-    <div className="App">
-      <h1>Welcome to the new bradyanderson.tech</h1>
-      <p>{!data ? "Loading..." : data}</p>
-    </div>
+    <>
+      <Container
+        fluid
+        className="App"
+        style={{
+          background: '#263238',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Row className="justify-content-center text-center">
+          <Col>
+            <Image
+              className="rounded mx-auto d-block"
+              src="/Logos/logo1024-3D-invert.png"
+              style={{
+                width: '170px',
+                padding: '15px 0'
+              }}/>
+            <h1>Welcome to the new bradyanderson.tech</h1>
+            <p>More coming soon...</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
