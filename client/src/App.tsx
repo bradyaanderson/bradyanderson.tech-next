4ac8ed448822e2019bff0d479abec5d701a55bac
Brady Anderson <bradya48@gmail.com>
2021-07-20 23:41:55 -0500

    Make home page mobile responsive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,22 @@ import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./assests/stylesheets/App.scss";
+import Container from "react-bootstrap/Container";
 
 /** Root Content **/
 function App() {
   return (
-    <Row className="justify-content-center text-center">
-      <Col>
-        <Image
-          className="logo rounded mx-auto d-block"
-          src="/Logos/logo1024-3D-Blue.png"
-        />
-        <h1>Welcome to the new bradyanderson.tech.</h1>
-      </Col>
-    </Row>
+    <Container>
+      <Row className="justify-content-center text-center">
+        <Col>
+          <Image
+            className="logo rounded mx-auto d-block"
+            src="/Logos/logo1024-3D-Blue.png"
+          />
+          <h1 className="title">Welcome to the new bradyanderson.tech</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
